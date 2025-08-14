39e50d20c69ae16c5fcf6e59578a9a59cc4e3d67
SAGAR KUMAR DAS <108587278+Sagarkumardas2002@users.noreply.github.com>
2025-08-14 14:20:14 +0530

    Update AuthPage.jsx

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,3 +1,11 @@
+// Imports the useRecoilValue hook from Recoil.
+// This hook is used to read the current value of a Recoil atom or selector without setting it.
+// Imports the authScreenAtom we created earlier (stores 'login' or 'signup' state).
+
+// Defines a functional component named AuthPage.
+//   Reads the current value of authScreenAtom.
+//   authScreenState will be 'login' or 'signup' depending on the state.
+    
 import { useRecoilValue } from "recoil";
 import authScreenAtom from "../atoms/authAtom";
 import LoginCard from "../components/LoginCard";
